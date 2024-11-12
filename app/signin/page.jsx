5076ed0f7663b94a7b85bc922ff8e8b50cce9ffc
Christopher Shivers <chrisshivers55@gmail.com>
2024-11-12 17:05:31 -0600

    added email and magic code ui components

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,9 +1,19 @@
+'use client'
 import React from 'react'
+import SignIn from './components/SignIn'
+import { Divider } from 'antd';
+import friend from '../img/friends.jpg'
+import Image from 'next/image';
 
 function page() {
   return (
-    <div>
-      
+    <div className='h-[100vh] flex'>
+      <div className='border-r-[1px] w-[1000px] flex justify-center items-center'>
+        <div className='w-full px-16'><SignIn /></div>
+      </div>
+      <div className='w-full hidden md:block'>
+        <Image src={friend} alt='friends' className='object-cover h-full w-full'/>
+      </div>
     </div>
   )
 }
