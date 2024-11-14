58da49cefd3804a40ca557eb3efa6da027f5c0fd
Christopher Shivers <chrisshivers55@gmail.com>
2024-11-14 01:14:56 -0600

    added authenticated users email, for signin verification

--- app/page.js
+++ app/page.js
@@ -1,8 +1,18 @@
+'use client'
 import Image from "next/image";
+import { useAuthStore } from "./state-management/State";
+import {db} from './utils/db'
+import Link from "next/link";
 
 export default function Home() {
+  //const {user} = useAuthStore()
+  
+  const {isLoading, user, error} = db.useAuth()
+  console.log(user)
   return (
     <div className="">
+      {user ? user.email: <h1>null</h1>}
+      <Link href='/signin'>click signin</Link>
     </div>
   );
 }
